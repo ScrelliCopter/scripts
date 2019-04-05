e4068b2379a97339718830894507c1ec9e274bd0
a dinosaur <nick@a-dinosaur.com>
2019-04-05 16:55:42 +1100

    hi

---
+++ iobench.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DEVICE="$1"
+
+
+do_ping() {
+    ioping -RD "$@" "$DEVICE" | grep 'requests completed' | cut -d ',' -f 3,4
+}
+
+echo -n "4K rand read:        "
+do_ping -s 4K
+
+echo -n "128K rand read:      "
+do_ping -s 128K
+
+echo -n "1M rand read:        "
+do_ping -s 1M
+
+######################
+####### WRITES #######
+######################
+
+echo -n "4K rand write:       "
+do_ping -WWW -s 4K
+
+echo -n "128K rand write:     "
+do_ping -WWW -s 128K
+
+echo -n "1M rand write:       "
+do_ping -WWW -s 1M
